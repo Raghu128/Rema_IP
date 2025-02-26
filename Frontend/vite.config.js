d4200980a1dededbd69fbb37e4ee0ad771ab3c26
Raghu Kumar <raghukumar@Raghus-MacBook-Air.local>
2025-02-26 21:48:31 +0530

    change in vite.congig for directory name problem

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on the current mode (development, production, etc.)
@@ -10,6 +11,11 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': env.VITE_BACKEND_URL,
       },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     },
     plugins: [react()],
   }
