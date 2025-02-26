51a94505654856902956aa27fd51dc69a58aedff
Raghu Kumar <raghukumar@Raghus-MacBook-Air.local>
2025-02-26 19:42:36 +0530

    add all updates for deploy test

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  
-  server: {
-    proxy: {
-      '/api': process.env.VITE_BACKEND_URL || 'http://localhost:3000',
-    }
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    server: {
+      proxy: {
+        '/api': env.VITE_BACKEND_URL,
+      },
+    },
+    plugins: [react()],
+  }
 })
